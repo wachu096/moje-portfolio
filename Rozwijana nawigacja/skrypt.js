a032dcb50a99c6c2581801a25cf76bb8d341e24a
wachu096 <40560466+wachu096@users.noreply.github.com>
2022-12-14 19:39:49 +0100

    Delete Rozwijana nawigacja directory

--- Rozwijana nawigacja/skrypt.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$(".btnOpen").click(function(){
-        $(".sidenav").css("width", "250px");
-    });
-   
-    $(".closebtn").click(function(){
-        $(".sidenav").css("width", "0"); 
-    });
-});
